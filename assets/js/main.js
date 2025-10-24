d2c9453125f1a2cb076eb13d4fdcf8903a80d955
nm <user2@nakanomisakinoMacBook-Air.local>
2025-10-24 19:05:29 +0900

    style slick 設定

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".slider").slick({
+  $(".mv__slider").slick({
     autoplay: true,
     dots: true,
     arrows: false,
@@ -10,3 +10,74 @@ $(function () {
     speed: 900,
   });
 });
+
+$(function () {
+  var $slider = $(".style__slider");
+  var slickInitialized = false; // 初期化フラグ
+
+  function initSlick() {
+    var winWidth = $(window).width();
+
+    if (winWidth <= 767) {
+      if (!slickInitialized) {
+        $slider.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: true,
+          arrows: false,
+          dots: false,
+          autoplay: true,
+          autoplaySpeed: 2500,
+        });
+        slickInitialized = true;
+      }
+    } else {
+      if (slickInitialized) {
+        $slider.slick("unslick"); // slickを破棄
+        slickInitialized = false;
+      }
+    }
+  }
+
+  // 初回ロード時
+  initSlick();
+
+  // リサイズ時もチェック
+  $(window).on("resize", function () {
+    initSlick();
+  });
+});
+
+// $(function () {
+//   $(".style__slider").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     infinite: true,
+//     dots: false,
+//     arrows: true,
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+//     variableWidth: true,
+//   });
+// });
+
+// ハンバーガーメニュー
+$(function () {
+  $(".btn-trigger").on("click", function () {
+    $(this).toggleClass("active");
+  });
+});
+// nav opne
+(function ($) {
+  var $nav = $("#navArea");
+  var $btn = $(".btn-trigger");
+  var open = "open"; // class
+  // menu open close
+  $btn.on("click", function () {
+    if (!$nav.hasClass(open)) {
+      $nav.addClass(open);
+    } else {
+      $nav.removeClass(open);
+    }
+  });
+})(jQuery);
